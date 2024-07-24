ffde63277d1733040be7435e22da84919f6d80ee
XuXingjian-H2O <168462136+XuXingjian-H2O@users.noreply.github.com>
2024-07-24 10:53:35 -0400

    Update this_is_bad_code.py

--- this_is_bad_code.py
+++ this_is_bad_code.py
@@ -1,5 +1,3 @@
-BadName =          123
-
-
-
-insane_calculation_format = 1+2+ (3//4)  -BadName
+no_longer_bad_name = 123
+sane_calculation_format = 1 + 2 + (3 // 4) - 0
+print(sane_calculation_format)
