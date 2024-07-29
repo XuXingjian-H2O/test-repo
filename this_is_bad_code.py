2acfaae20e3f20a11a458b0a5c4162c86c9d4d81
XuXingjian-H2O <168462136+XuXingjian-H2O@users.noreply.github.com>
2024-07-29 09:42:48 -0400

    Update this_is_bad_code.py

--- this_is_bad_code.py
+++ this_is_bad_code.py
@@ -2,9 +2,9 @@
 DOC
 """
 BadName                           = -404
-for   
-  bad
-in range(4):
+for bad in [
+  range(4)
+]:
   print(1)
 no_longer_bad_name = 123
 sane_calculation_format = 1 + 2 + (3 // 4) - 0
